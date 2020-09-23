7c4fb0b6bea537ed6b2158ad83c53684df1c836a
Bob Vawter <bobvawter@msn.com>
2020-09-23 15:05:14 -0400

    codewars daily

--- gitfill.sh
+++ gitfill.sh
@@ -2,10 +2,10 @@
 
 touch test.txt
 git add .
-git commit --date " 150 days ago" -m "codewars daily"
+git commit --date " 180 days ago" -m "codewars daily"
 git push -u origin main
 rm test.txt
 git add .
-git commit --date " 150 days ago" -m "codewars daily"
+git commit --date " 180 days ago" -m "codewars daily"
 git push -u origin main
 
