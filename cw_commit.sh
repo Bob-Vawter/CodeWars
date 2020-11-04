6d593e04cca679190d5c72526f88b56e37af5860
Bob Vawter <bobvawter@msn.com>
2020-11-04 15:48:00 -0500

    daily codewar

--- cw_commit.sh
+++ cw_commit.sh
@@ -2,9 +2,9 @@
 
 touch test.txt
 git add .
-git commit --date " 133 days ago" -m 'daily codewar'
+git commit --date " 135 days ago" -m 'daily codewar'
 git push -u origin main
 rm test.txt
 git add .
-git commit --date " 134 days ago" -m 'daily codewar'
+git commit --date " 136 days ago" -m 'daily codewar'
 git push -u origin main
