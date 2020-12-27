b6f76dd99563a85f59f71e825a8eeead51f0548b
Bob Vawter <bobvawter@msn.com>
2020-12-26 20:09:34 -0500

    cw daily

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-// This problem takes its name by arguably the most important event in the life of the ancient historian Josephus: according to his tale, he and his 40 soldiers were trapped in a cave by the Romans during a siege.
-//
-// Refusing to surrender to the enemy, they instead opted for mass suicide, with a twist: they formed a circle and proceeded to kill one man every three, until one last man was left (and that it was supposed to kill himself to end the act).
-//
-// Well, Josephus and another man were the last two and, as we now know every detail of the story, you may have correctly guessed that they didn't exactly follow through the original idea.
-//
-// You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
-// kyu 5
+function duplicateEncode(word){
+    word = word.toLowerCase().split('')
+    var res = []
+    var count = 0
+    word.forEach((a)=>{
+      count = 0
+      word.forEach((b)=>{
+      if(a == b){
+        count++
+      }
+      })
+      res.push(count)
+    })
+    res.forEach((a,i)=>{
+      res[i] = a===1?'(':')'
+    })
+    return res.join('')
+}
