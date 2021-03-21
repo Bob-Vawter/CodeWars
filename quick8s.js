17c8a188c5202a27a88a640c5b1325bf7feee08d
Bob Vawter <bobvawter@msn.com>
2021-03-21 09:51:34 -0400

    codewars daily

--- quick8s.js
+++ quick8s.js
@@ -3,7 +3,21 @@
 function maps(x){
   return x.map(a=>a*2)
 }
+
 function century(year) {
   // Finish this :)
   return Math.ceil(year/100)
+}
+
+function countPositivesSumNegatives(input) {
+  var result = []
+  if(input == null){
+    return result
+  }
+  if(input.length>0){
+    result.push(input.filter(a=>a>0).length)
+    result.push(input.filter(a=>a<0).reduce((a,b)=>a+b,0))
+    console.log(result)
+  }
+  return result
 }
