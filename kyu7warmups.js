395f95203b9efa3c44f1fad3c00274a0c8a983fc
Bob Vawter <bobvawter@msn.com>
2021-03-22 10:57:16 -0400

    codewars daily

--- kyu7warmups.js
+++ kyu7warmups.js
@@ -1,1 +1,11 @@
 //kyu 7 quick warmups
+
+function disemvowel(str) {
+  var vowels = ['a','e','i','o','u']
+  return str.split('').filter(a=>!vowels.includes(a.toLowerCase())).join('');
+}
+
+function getMiddle(s)
+{
+  return s.length%2==1?s.charAt(Math.floor(s.length/2)):s.charAt(s.length/2-1)+s.charAt(s.length/2)
+}
