aa95316f6abdf0abef8384af1409c43d14898266
Bob Vawter <bobvawter@msn.com>
2021-03-25 11:49:52 -0400

    codewars daily

--- quicksevens.js
+++ quicksevens.js
@@ -5,3 +5,9 @@ function DNAStrand(dna){
   dna = dna.split('').map(a=>a==='G'?a.replace('G','C'):a.replace('C','G')).join('')
   return dna
 }
+
+// var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+//
+// function DNAStrand(dna){
+//   return dna.split('').map(function(v){ return pairs[v] }).join('');
+// }
