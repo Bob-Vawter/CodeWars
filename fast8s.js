220dae4e8f1633fa3fd456939f0db1d5181c0d13
Bob Vawter <bobvawter@msn.com>
2021-03-27 13:26:44 -0400

    codewars daily

---
+++ fast8s.js
@@ -0,0 +1,26 @@
+// kyu quick 8s
+
+function find_average(arr) {
+  return arr.length>0?arr.reduce((a,b)=>a+b)/arr.length:0
+}
+
+function areYouPlayingBanjo(name) {
+  return name.charAt(0)==='R'||name.charAt(0)==='r'?name +" plays banjo":name + " does not play banjo"
+}
+
+function arrayPlusArray(arr1, arr2) {
+  var redux = (a,b) => a+b
+  return arr1.reduce(redux) + arr2.reduce(redux)
+}
+
+function sum (num) {
+  return num.length>0?num.reduce((a,b)=>a+b):0
+};
+
+function monkeyCount(n) {
+  var arr = []
+  for(let i = 1; i<=n;i++){
+    arr.push(i)
+  }
+  return arr
+}
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-//kyu 5
+// kyu quick 8s
+
+function find_average(arr) {
+  return arr.length>0?arr.reduce((a,b)=>a+b)/arr.length:0
+}
