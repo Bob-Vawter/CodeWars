896d5424246f58a9ab0c65392289381222fdbd7d
Bob Vawter <bobvawter@msn.com>
2021-03-28 10:09:12 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
 //kyu 7
-
-var buy = function(x, arr){
-  var result = []
-  arr.forEach((item,i)=>{
-    for(let j = i+1; j<arr.length; j++){
-     if(item+arr[j]===x){
-       result.push([i,j])
-     }
-    }
-  })
-  return result.length>0?result[0]:null
-};
