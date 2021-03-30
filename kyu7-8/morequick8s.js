99253acd4c8cd4a0a5b52a48cfb928ee0af5e2bb
Bob Vawter <bobvawter@msn.com>
2021-03-30 11:55:44 -0400

    codewars daily

---
+++ kyu7-8/morequick8s.js
@@ -0,0 +1,40 @@
+//kyu 8 fast 8s
+
+const areaOrPerimeter = function(l , w) {
+  return l === w?l*w: 2*l+2*w
+};
+
+var humanYearsCatYearsDogYears = function(hY) {
+  var arr = [hY,0,0]
+
+  for(let i = 1;i<=hY;i++){
+    arr[1] += i===1?15:(i===2?9:4)
+    arr[2] += i===1?15:(i===2?9:5)
+  }
+
+  return arr;
+}
+
+const flip=(d, a)=>{
+  return d === 'R'?a.sort((a,b)=>a-b):a.sort((a,b)=>b-a)
+}
+
+function aspectRatio(x,y){
+  return [Math.ceil(y*16/9),y]
+}
+
+function shark(pD, sD, yS, sS, dolphin){
+  return dolphin?(pD/yS<sD/(sS/2)?'Alive!':"Shark Bait!"):(pD/yS<sD/sS?'Alive!':"Shark Bait!")
+}
+
+function shark(pD, sD, yS, sS, dolphin){
+  return dolphin?(pD/yS<sD/(sS/2)?'Alive!':"Shark Bait!"):(pD/yS<sD/sS?'Alive!':"Shark Bait!")
+}
+
+function checkAlive (health) {
+  if (health <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
