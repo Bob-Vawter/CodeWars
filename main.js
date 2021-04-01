9efae2d484143f8935d51d3dafabead4dc124b00
Bob Vawter <bobvawter@msn.com>
2021-04-01 11:13:18 -0400

    codewars daily

--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 String.prototype.toAlternatingCase = function () {
   return this.split('').map(a=>a==a.toLowerCase()?a.toUpperCase():a.toLowerCase()).join('')
 }
+
+const capitalizeWord = (word) =>
+ word.split('').map((a,i)=>i==0?a.toUpperCase():a).join('')
