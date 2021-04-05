1d7e554a8ea6a2323390a655b5f9de9bc9879985
Bob Vawter <bobvawter@msn.com>
2021-04-05 13:20:17 -0400

    codewars daily

---
+++ kyu7-8/pricemangos.js
@@ -0,0 +1,5 @@
+//kyu 8
+
+function mango(quantity, price){
+ return Math.floor(quantity/3)*2*price+quantity%3*price
+}
--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 //kyu 8
-
-function integrate(coefficient, exponent) {
-  var newEx = exponent + 1,newBase = coefficient/newEx
-  return newBase + 'x^' + newEx
-}
