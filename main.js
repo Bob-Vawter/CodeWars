ae6a7b8a6d4398980e05aa72f06e2f9d85591bec
Bob Vawter <bobvawter@msn.com>
2021-04-16 08:55:38 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-//kyu 6
-//english beggars
 
-function beggars(values, n){
-  var result = new Array(n).fill(0)
-  for(let i = 0; i<values.length; i++){
-    result[i%n]+=values[i]
-  }
-  return result
-}
