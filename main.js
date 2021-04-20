d523a37911817561bb888ea6e912521139116ee7
Bob Vawter <bobvawter@msn.com>
2021-04-20 15:15:21 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 //kyu 8
-//exluding vat
-//return price without vat
-function excludingVatPrice(price){
-  console.log(price)
-  if(price == null){
-    return -1
-  }
-  return +(price/1.15).toFixed(2)
-}
+//
