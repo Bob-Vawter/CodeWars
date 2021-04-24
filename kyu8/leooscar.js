d369db1b894738bd7d70cf33a560c48ad6ed3819
Bob Vawter <bobvawter@msn.com>
2021-04-24 14:38:16 -0400

    cw daily

---
+++ kyu8/leooscar.js
@@ -0,0 +1,5 @@
+//kyu 8
+//leo oscar
+function leo(oscar){
+ return oscar == 88 ?'Leo finally won the oscar! Leo is happy':(oscar == 86?'Not even for Wolf of wallstreet?!':(oscar>88?'Leo got one already!':'When will you give Leo an Oscar?'))
+}
