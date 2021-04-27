1047cbe494f09b6114f4c43e660903df2ee45a55
Bob Vawter <bobvawter@msn.com>
2021-04-27 16:52:06 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
 //kyu 8
-//validate code with simple regex
-// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
-//
-// You can assume the input will always be a number.
-function validateCode (code) {
-  return code.toString().charAt(0).match(/[1-3]/)!=null
-}
-//mine
-
-
-function validateCode (code) {return /^[123]/.test(code)}
-//top
-// ^ - begins with
-// .test() - regex test method
