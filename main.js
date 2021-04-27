264e00e7dd58fca25a7379008e9b64eb6bed7dc1
Bob Vawter <bobvawter@msn.com>
2021-04-27 16:35:47 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
 //kyu 8
-//finish guess the number games
-// Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+// The number n is Evil if it has an even number of 1's in its binary representation.
+// The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
 //
-// If the user tries to guess more than the limit, the function should throw an error.
-// If the user guess is right it should return true.
-// If the user guess is wrong it should return false and lose a life.
-// Can you finish the game so all the rules are met?
-class Guesser {
-  constructor(number, lives) {
-    this.number = number;
-    this.lives = lives;
-  }
-
-  guess(n) {
-    if(this.lives<=0){
-      throw 'Out of Lives'
-    }
-    if(n==this.number){
-      return true
-    }
-    this.lives--
-    return false;
-  }
-}
+// The number n is Odious if it has an odd number of 1's in its binary representation.
+// The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+//
+// You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+//
+// good luck :)
+//evil or odious
