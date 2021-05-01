5cbd12c5c47888f3720187019b0bd176c0b7db95
Bob Vawter <bobvawter@msn.com>
2021-05-01 12:43:44 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 //kyu 8
-//Training JS #32: methods of Math---round() ceil() and floor()
-function roundIt(n){
-  var magic = n.toString().split('.')
- return magic[0].length==magic[1].length?Math.round(n):(magic[0].length>magic[1].length?Math.floor(n):Math.ceil(n))
-}
+//
