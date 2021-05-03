2e8407c338a1e3b54bc43d260ffc760ecb133add
Bob Vawter <bobvawter@msn.com>
2021-05-03 12:42:50 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 //kyu 8
-//Pirates!! Are the Cannons ready!??
-const cannonsReady = (gunners) => {
-  for(let i in gunners){
-    if(gunners[i]=='nay'){
-      return 'Shiver me timbers!'
-    }
-  }
-  return 'Fire!'
-}
+//
