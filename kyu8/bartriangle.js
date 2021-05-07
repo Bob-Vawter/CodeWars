2c085c320299d3fd09a2278f9fb496d883666f6a
Bob Vawter <bobvawter@msn.com>
2021-05-07 07:35:43 -0400

    codewars daily

--- kyu8/bartriangle.js
+++ kyu8/bartriangle.js
@@ -1,8 +1,2 @@
 //kyu 8
-//Localize The Barycenter of a Triangle
-function barTriang(p1, p2, p3){
-  var result = []
-  result.push(+((p1[0]+p2[0]+p3[0])/3).toFixed(4))
-  result.push(+((p1[1]+p2[1]+p3[1])/3).toFixed(4))
-  return result
-}
+//
