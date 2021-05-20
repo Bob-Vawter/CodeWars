982fdf051bbf18985e53099dd40af7391bd54828
Bob Vawter <bobvawter@msn.com>
2021-05-20 14:57:00 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 //kyu 8
-//Training JS #14: Methods of Number object--toString() and toLocaleString()
-function colorOf(r,g,b){
-  var rgb = '#'
-  rgb += r.toString(16).length>1?r.toString(16):'0'+r.toString(16)
-  rgb += g.toString(16).length>1?g.toString(16):'0'+g.toString(16)
-  rgb += b.toString(16).length>1?b.toString(16):'0'+b.toString(16)
-  return rgb
-}
---
+++ strings.js
@@ -0,0 +1,18 @@
+// Please research the following string methods:
+// charAt
+
+
+// charCodeAt
+// concat
+// includes
+// indexOf
+// match
+// repeat
+// replace
+// search
+// slice
+// split
+// substr
+// toLowerCase
+// toUpperCase
+// trim
