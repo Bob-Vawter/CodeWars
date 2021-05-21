c3e39925357675fa6be98f08721a6b69eec8dca4
Bob Vawter <bobvawter@msn.com>
2021-05-20 20:46:01 -0400

    codewars daily

---
+++ kyu8/playerrankup.js
@@ -0,0 +1,5 @@
+//kyu 8
+//Online RPG: player to qualifying stage?
+function playerRankUp (points){
+  return points < 100 ? false : "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.";
+}
