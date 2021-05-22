3fff28eac82d56b7e80ebe31a991eee6d616b077
Bob Vawter <bobvawter@msn.com>
2021-05-22 15:12:08 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
 //kyu 8
-//Job Matching #1
-function match(candidate, job) {
-  if(!candidate.minSalary || !job.maxSalary){
-    throw 'Error'
-  }
-  return candidate.minSalary*.90<=job.maxSalary
-}
+//
--- strings.js
+++ strings.js
@@ -1,4 +1,5 @@
 // Please research the following string methods:
+
 // charAt
 
 
@@ -13,6 +14,21 @@
 // slice
 // split
 // substr
+
 // toLowerCase
+// returns string with all characters to lower case
+var str = 'My Random String'
+str = str.toLowerCase()
+//returns 'my random string'
+
 // toUpperCase
+// returns string with all characters to upper case
+var str = 'My Random String'
+str = str.toLowerCase()
+//returns 'MY RANDOM STRING'
+
 // trim
+// trims whitespace from beginning and end of STRING
+var str = ' my random string '
+str = str.trim()
+//str now equals 'my random string'
