1f618ebd0f326691542621f8ac55cf4732c49539
Bob Vawter <bobvawter@msn.com>
2021-05-25 13:32:53 -0400

    codewars daily

--- dev100/strings.js
+++ dev100/strings.js
@@ -33,24 +33,36 @@ str.charAt(4)
 
 // match
 // The match() method retrieves the result of matching a string against a regular expression.
-
+var regex = /[A-Z]/g
+'This Cool String'.match(regex) // returns ['T','C','S']
+var regex2 = /A-E/ig
+"This is a Cool String".match(regex2) //returns ['a','C']
 
 // repeat
-
+// The repeat() method constructs and returns a new string which contains the specified number of copies of the string on which it was called, concatenated together.
+'Repeat'.repeat(2) // returns 'RepeatRepeat'
 
 // replace
-
+// The replace() method returns a new string with some or all matches of a pattern replaced by a replacement. The pattern can be a string or a RegExp, and the replacement can be a string or a function to be called for each match. If pattern is a string, only the first occurrence will be replaced.
+//
+// The original string is left unchanged.
+'Repeat'.replace('eat','lace') //return 'replace'
 
 // search
+// The search() method executes a search for a match between a regular expression and this String object.
 
 
 // slice
-
+// The slice() method extracts a section of a string and returns it as a new string, without modifying the original string.
+'This is a cool string'.slice(5) //returns 'is a cool string'
 
 // split
-
+// The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.  The division is done by searching for a pattern; where the pattern is provided as the first parameter in the method's call.
+'This is a cool string'.split(' ') //returns ['This','is','a','cool','string']
 
 // substr
+// The substr() method returns a portion of the string, starting at the specified index and extending for a given number of characters afterwards.
+'This is a cool string'.substr(5,4)  //returns 'is a'
 
 // toLowerCase
 // returns string with all characters to lower case
