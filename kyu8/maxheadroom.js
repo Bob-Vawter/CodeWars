6d67a48ff96861fe93ca5e46f0fa9da373b0d072
Bob Vawter <bobvawter@msn.com>
2021-06-21 19:03:30 -0400

    cw daily

---
+++ kyu8/maxheadroom.js
@@ -0,0 +1,13 @@
+//kyu 8
+// Max Headroom and JavaScript style
+function getMax1()
+{
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2()
+{
+// Have your return object on same line
+return {name: 'Max Headroom'};
+}
--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 //kyu 8
-// definitely not an 8
-// Implement Array.prototype.filter()
-
-Array.prototype.filter = function (func) {
-  var result = []
-  for(let i = 0;i<this.length;i++){
-    if(func(this[i])){
-      result.push(this[i])
-    }
-  }
-  return result
-}
+// 
