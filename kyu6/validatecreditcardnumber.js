81742f25f056a21b05a4984a320fc4e65dac4d29
Bob Vawter <bobvawter@msn.com>
2021-07-01 22:18:09 -0400

    codewars daily

--- kyu6/validatecreditcardnumber.js
+++ kyu6/validatecreditcardnumber.js
@@ -1,6 +1,7 @@
 // In this Kata, you will implement the Luhn Algorithm, which is used to help validate credit card numbers.
 //
 // Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not.
+//kyu 6
 
 function validate(n){
   var card = n.toString().split('').map(a=>+a)
