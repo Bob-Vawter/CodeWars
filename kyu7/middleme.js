d3bf028367fef4a42b110520f1fa20b92b2bbc9d
Bob Vawter <bobvawter@msn.com>
2021-07-17 13:17:35 -0400

    cw daily

---
+++ kyu7/middleme.js
@@ -0,0 +1,5 @@
+function middleMe(N, X, Y){
+   return N%2==0?(Y.repeat(N/2)+X+Y.repeat(N/2)):X
+}
+//kyu 7
+//middle me
---
+++ main.js
@@ -0,0 +1,11 @@
+function correctness(bobsDecisions, expertDecisions) {
+  var result = 0
+  bobsDecisions.forEach((ele,i)=>{
+    if(ele==expertDecisions[i]){
+      result+=1
+    } else if(ele=='?'||expertDecisions[i]=='?'){
+      result+=.5
+    }
+  })
+  return result
+}
