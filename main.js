e2feac10978f8ad75d0af5d5050cbd64d00baf61
Bob Vawter <bobvawter@msn.com>
2021-07-17 13:35:38 -0400

    cw daily

--- main.js
+++
@@ -1,9 +0,0 @@
-function multiplicationTable(row,col){
-  var result = []
-  for(let i = 1;i<=row;i++){
-    result.push(Array.from(Array(col).keys()).map(a=>(a+1)*i))
-  }
-  return result
-}
-//kyu 6
-//multiplicationTable
