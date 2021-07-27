3fc27baa7b0818ed41e8cbe989f6183c1486f2ed
Bob Vawter <bobvawter@msn.com>
2021-07-27 16:14:13 -0400

    cw daily

---
+++ kyu7/generatesquare.js
@@ -0,0 +1,5 @@
+//kyu 7
+//make square
+function generateShape(n){
+  return ("+".repeat(n)+"\n").repeat(n).trim()
+}
--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 //kyu 7
-//sum of integers in string
-function sumOfIntegersInString(s){
-  console.log(s)
-  s = s.match(/\d+/g)
-  return s?s.reduce((a,b)=>a+(+b),0):0
-}
+//
