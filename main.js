e57575299942787eb02be09944432397900a9a90
Bob Vawter <bobvawter@msn.com>
2021-08-08 13:35:05 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-//kyu 5
-//currying
-//a chain adding function
-function add(n){
-  var curryAdd = function(x) {
-    return add(n + x);
-  };
-
-  curryAdd.valueOf = function() {
-    return n;
-  };
-
-  return curryAdd;
-}
+//kyu 
