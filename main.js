4312ce6c8c4d7a51b3b2f3e283d7e2eeff69e9ba
Bob Vawter <bobvawter@msn.com>
2021-08-21 15:26:53 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-//kyu 6
-//23 Matches. Or more..
-const createBot = (X, Y) => {
-let myBot = {
-   name: "Winner",
-   makeMove: matchesStillOnTable => {
-     let matchToTake = 0
-     if(matchesStillOnTable%(Y+1)==0){
-       matchToTake = Y
-     } else {
-       matchToTake = matchesStillOnTable%(Y+1)-1
-     }
-     return matchToTake
-   }
-}
-
-   return myBot;
-}
-
-
-//X > Y+1 ? (X-1)%Y : X-1
+//kyu 7
+//
