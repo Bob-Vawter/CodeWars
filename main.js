cb072276ff4e964a9bdc564e33850e55f9d6b791
Bob Vawter <bobvawter@msn.com>
2021-08-22 16:51:11 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,17 +1,2 @@
 //kyu 7
-//loopArr
-function loopArr(arr, direction, steps) {
-  if(direction == 'left'){
-    var pH
-    for(let i = 0;i<steps;i++){
-      pH = arr.shift()
-      arr.push(pH)
-    }
-  } else {
-    for(let i = 0;i<steps;i++){
-      pH = arr.pop()
-      arr.unshift(pH)
-    }
-  }
-  return arr
-}
+//
