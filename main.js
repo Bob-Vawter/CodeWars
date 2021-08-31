d04f6a10b0b1bab482cc50e2bf807d06302a4fe4
Bob Vawter <bobvawter@msn.com>
2021-08-31 12:44:39 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
 //kyu 6
-//pyramid array
-function pyramid(n) {
-  var newArray = [];
-
-  if(n === 0) {
-    return [];
-  } else if(n > 0) {
-
-    for(let i = 0; i < n; i++) {
-      newArray[i] = new Array();
-      for(let j = 0; j <= i; j++) {
-        newArray[i][j] = 1;
-      }
-    }
-  }
-
-  return newArray;
-}
+//
