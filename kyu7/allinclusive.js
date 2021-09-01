740dd63b227927075eb4229a439509b454bb74ee
Bob Vawter <bobvawter@msn.com>
2021-09-01 13:25:50 -0400

    codewars daily

---
+++ kyu7/allinclusive.js
@@ -0,0 +1,10 @@
+//kyu 7
+//all inclusive
+function containAllRots(str, arr) {
+  for (var i = 0; i < str.length; i++) {
+    if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+      return false
+    }
+  }
+  return true
+}
