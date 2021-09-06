af478714fd4329e6c9dfb155a2b8c39c91d8322d
Bob Vawter <bobvawter@msn.com>
2021-09-06 18:49:47 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 //kyu 7
-//pairs of integers from m to n
-function generatePairs(m, n) {
-  var result = []
-  for(let i = m;i<=n;i++){
-    for(let j=i;j<=n;j++){
-      result.push([i,j])
-    }
-  }
-  return result
-}
+//
