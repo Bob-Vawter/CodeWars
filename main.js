f5ba1dfa0043004ea563584452d305704c8392ab
Bob Vawter <bobvawter@msn.com>
2021-09-07 19:15:43 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 //kyu 7
-//find the middle element
-const gimme = (inputArray) => inputArray.indexOf(midNum(inputArray))
-const midNum = (arr) => {
-  if((arr[0]>arr[1]&&arr[0]<arr[2])||(arr[0]<arr[1]&&arr[0]>arr[2])){
-    return arr[0]
-  } else if((arr[1]>arr[0]&&arr[1]<arr[2])||(arr[1]<arr[0]&&arr[1]>arr[2])){
-    return arr[1]
-  } else {
-    return arr[2]
-  }
-}
+//
