8b4a7ad573f914361c58226648f3b9d571a354b0
Bob Vawter <bobvawter@msn.com>
2021-09-16 15:17:53 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 //kyu 7
-//factorial factory
-//This function should return n!
-function factorial (n) {
-  if(n<0){
-    return null
-  } else if(n==0){
-    return 1
-  }
-  return n*factorial(n-1)
-}
+//
