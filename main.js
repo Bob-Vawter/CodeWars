bf2d765bcf641e7b268879cfdf264887f7486927
Bob Vawter <bobvawter@msn.com>
2021-09-17 20:46:27 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 //kyu 7
-//moves in squared string
-function vertMirror(strng) {
-    return strng.split('\n').map(a=>a.split('').reverse().join('')).join('\n')
-}
-function horMirror(strng) {
-    return strng.split('\n').reverse().join('\n')
-}
-function oper(fct, s) {
-    return fct(s)
-}
+//
