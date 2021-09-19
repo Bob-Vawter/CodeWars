d102a337453d883ba7aeab6a8583a7bee660b4fb
Bob Vawter <bobvawter@msn.com>
2021-09-18 20:55:44 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 //kyu 7
-//
+//Get key/value pairs as arrays
+function keysAndValues(data){
+  var map=[]
+  var vals=[]
+  for(let ele in data){
+    map.push(ele)
+    vals.push(data[ele])
+  }
+  return [map,vals]
+}
