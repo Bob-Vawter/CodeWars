53aabc2a38dd62724f57a2118d0a5d15ad2d07a8
Bob Vawter <bobvawter@msn.com>
2021-09-25 16:22:58 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 //kyu 7
-//Find the Speedcuber's times!
-function cubeTimes(times) {
-  var fast = times.sort((a,b)=>a-b).shift()
-  times.pop()
-  return [+(times.reduce((a,b)=>a+b)/3).toFixed(2),fast]
-}
+//
