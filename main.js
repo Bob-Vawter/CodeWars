37793652d766bfbee6808011c7f36d9c1f6811ad
Bob Vawter <bobvawter@msn.com>
2021-09-28 16:46:05 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 //kyu 7
-//string prefix and suffix
-function solve(s){
-    let run = Math.floor(s.length/2)
-    while(run){
-      if(s.slice(0,run)==s.slice(s.length-run)){
-        return run
-      }
-      run--
-    }
-    return 0
-}
+//
