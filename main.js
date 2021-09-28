40569ac767f2dda0a345d6962fb72b433516d3cc
Bob Vawter <bobvawter@msn.com>
2021-09-28 16:50:35 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
 //kyu 7
-//which section did you scroll to
-// return an index of section or -1 if past last section
-// scrollY is a positive integer and sizes is an array of positive integers
-function getSectionIdFromScroll(scrollY,sizes){
-  var sum = 0
-  for(let i = 0;i<sizes.length;i++){
-    sum+=sizes[i]
-    if(scrollY<sum){
-      return i
-    }
-  }
-  return -1
-}
+//
