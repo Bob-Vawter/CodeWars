70ef850912dda5042d4131d799dbdf7d2fb3bc09
Bob Vawter <bobvawter@msn.com>
2021-09-29 19:01:05 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 //kyu 7
-//keystroking
-function numKeyStrokes(text) {
-  var shift = /[\~\!\@\#\$\%\^\&\*\(\)\_\+\|\{\}\:\"\<\>\?A-Z]/
-  return text.split('').map(a=>shift.test(a)?2:1).reduce((a,b)=>a+b,0)
-}
+//
