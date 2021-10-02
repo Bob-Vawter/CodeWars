d663410fcdf9c796105f09512d777b26bc7bdc70
Bob Vawter <bobvawter@msn.com>
2021-10-02 16:32:53 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 //kyu 7
-//apples distribution
-function applesDistribution(a, bC, mR) {
-  //coding and coding..
-  var result = 0
-  for(let i = 1;i<=bC;i++){
-    if(a%i<=mR){
-      result++
-    }
-  }
-  return result
-}
+//
