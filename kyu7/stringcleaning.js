ffaab3a43c63b5b2e8a13631e188a5096e91aa18
Bob Vawter <bobvawter@msn.com>
2021-10-19 16:44:27 -0400

    codewars daily

---
+++ kyu7/stringcleaning.js
@@ -0,0 +1,5 @@
+//kyu 7
+//string clean
+function stringClean(s){
+  return s.replace(/[0-9]/g,'')
+}
--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-//kyu6
-//array combinations
-function solve(arr) {
-  return arr.map(a=>[...new Set(a)]).reduce((acc,a)=>acc*a.length,1)
-};
+//kyu 7
+//
