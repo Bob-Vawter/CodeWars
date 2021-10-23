4d684adb00a0b8cd143e9fb5f73fe04c48fdd905
Bob Vawter <bobvawter@msn.com>
2021-10-23 15:55:49 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 //kyu 7
-//minimum steps
-function minimumSteps(numbers, value){
-  var sum = 0
-  var count = -1
-  numbers = numbers.sort((a,b)=>a-b)
-  for(let i = 0;sum<value&&i<numbers.length;i++){
-    sum+=numbers[i]
-    count++
-  }
-  return count
-}
+//
