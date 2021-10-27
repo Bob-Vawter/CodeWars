d29e21102a58e8a186d7ac260e40a91060b687d5
Bob Vawter <bobvawter@msn.com>
2021-10-27 12:48:34 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
 //kyu 7
-//the pony express
-function riders(stations) {
-  var freshHorse = 100
-  var horseCount = 1
-  for(let i = 0;i<stations.length;i++){
-    freshHorse-=stations[i]
-    if(freshHorse<0){
-      freshHorse=100-stations[i]
-      horseCount++
-    }
-  }
-  return stations.length?horseCount:0
-}
+//
