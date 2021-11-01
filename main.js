6c87681221e29d8af93492e9725eb6e953b13040
Bob Vawter <bobvawter@msn.com>
2021-11-01 17:18:39 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 //kyu 7
-// find the middle element
-var gimme = function (iA) {
-  var sA = []
-  for(let i = 0;i<3;i++){
-    sA.push(iA[i])
-  }
-  return iA.indexOf(sA.sort((a,b)=>a-b)[1])
-};
-// cleaner
-const gimme = function (arr) {
-  return arr.indexOf([...arr].sort((x, y) => x > y)[1]);
-};
+//
