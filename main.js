93cfcb8ec4b66210b35f3c6a5b4fe74c9fc83b0c
Bob Vawter <bobvawter@msn.com>
2021-11-04 16:10:40 -0400

    codewars daily

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
 //kyu 7
-//are equally strong
-function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
-  var youS = yourLeft>yourRight?yourLeft:yourRight
-  var youW = yourLeft<yourRight?yourLeft:yourRight
-  var friendS = friendsLeft>friendsRight?friendsLeft:friendsRight
-  var friendW = friendsLeft<friendsRight?friendsLeft:friendsRight
-
-  return youS == friendS && friendW == youW
-}
+//
