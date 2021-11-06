2df02cfef1fbc8673d1040eac63d78d2bb92cb9c
Bob Vawter <bobvawter@msn.com>
2021-11-06 15:03:04 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 //kyu 7
-//
+//Hello World - Without Strings
+const helloWorld = () => {
+  var str = [72,101,108,108,111,44,32,87,111,114,108,100,33]
+  str = str.map(a=>String.fromCharCode(a))
+  return str.reduce((acc,ele)=>acc+ele)
+};
