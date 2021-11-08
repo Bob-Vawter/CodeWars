9b394d170696e2023ecbdf574709b094d45914ab
Bob Vawter <bobvawter@msn.com>
2021-11-08 15:02:24 -0500

    codewars daily

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 //kyu 7
-//
+// Exclamation marks series #8: Move all exclamation marks to the end of the sentence
+function remove(s){
+  var tail = s.length
+  s = s.split('!').join('')
+  return s+'!'.repeat(tail-s.length)
+}
