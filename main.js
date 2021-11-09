9e367e44c770569cadec2886e6baa947bda36702
Bob Vawter <bobvawter@msn.com>
2021-11-09 15:28:37 -0500

    codewars daily

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 //kyu 7
-//
+//consecutive letters
+function solve(s){
+    var s = s.split('').sort().map(a=>a.charCodeAt())
+    var count = 1
+    for(let i = 1;i<s.length;i++){
+      if(s[i]==s[i-1]+1){
+        count++
+      }
+    }
+  return count == s.length
+}
