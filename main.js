63f9ebcb158d165ca5a2f44ccb58b4663f8b2b8c
Bob Vawter <bobvawter@msn.com>
2021-11-10 13:18:07 -0500

    codewars daily

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
 //kyu 7
-//thinking and testing how many 'word'
-function testit(s){
-  s=s.toLowerCase()
-  var word=(s.match(/[word]/g)||[])
-  var count = 0
-  var j = 0
-  for(let i = 0;i<word.length;i++){
-    if(word[i]=='word'[j%4]){
-      j++
-      if(word[i]=='d'){
-        count++
-      }
-    }
-  }
-
-  return count
-}
+//
