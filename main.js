e8f8b63016fecee8a4b24e4606404e126e34f2a6
Bob Vawter <bobvawter@msn.com>
2021-11-12 14:48:49 -0500

    cw daily

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 //kyu 7
-//absent vowel
-function absentVowel(x){
-  x = x.toLowerCase()
-  x = x.match(/[aeiou]/g)
-  x = [...new Set(x)]
-  return !x.includes('a')?0:
-      !x.includes('e')?1:
-  !x.includes('i')?2:
-  !x.includes('o')?3:4
-}
+//
