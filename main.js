28519a26ae719bbeedb058b79c8171417bd62b8a
Bob Vawter <bobvawter@msn.com>
2022-03-09 17:00:43 -0500

    same case codewars

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-//kyu 8
-//check same case
-
-function sameCase(a, b){
-  return notLetter(a) || notLetter(b) ? -1 : upperLower(a) == upperLower(b) ? 1 : 0
-}
-
-function notLetter(a){
-  return !(/[A-Za-z]/).test(a)
-}
-
-function upperLower(a){
-  return a == a.toUpperCase()
-}
+//kyu 7
