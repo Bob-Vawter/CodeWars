1521b362ef933be62d6816225343e09406ede9e2
Bob Vawter <bobvawter@msn.com>
2022-03-31 17:21:19 -0400

    atm codewars

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 //kyu 7
-//
+//atm
+function solve(n,count = 0) {
+  return  n>=500?solve(n-500,++count):
+          n>=200?solve(n-200,++count):
+          n>=100?solve(n-100,++count):
+          n>=50?solve(n-50,++count):
+          n>=20?solve(n-20,++count):
+          n>=10?solve(n-10,++count):
+          n==0?count:-1
+}
