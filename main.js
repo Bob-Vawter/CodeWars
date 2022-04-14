225d3b7041b8917592445808cead54232dc6f92e
Bob Vawter <bobvawter@msn.com>
2022-04-14 17:25:27 -0400

    filter unused digits

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
 //kyu 7
-//filter unused digits
-function unusedDigits() {
-  var result = ''
-  for(num of arguments){
-    result += num.toString()
-  }
-  result=result.split('')
-  var finalSolution = ''
-  for(let i = 0;i<=9;i++){
-    if(!result.includes(i.toString())){
-      finalSolution += i.toString()
-    }
-  }
-  return finalSolution
-}
+//
