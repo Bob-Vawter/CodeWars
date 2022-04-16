30a528ad8166adbdee47d627ce5529c5ef45080e
Bob Vawter <bobvawter@msn.com>
2022-04-16 17:51:55 -0400

    moment of time in space

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
 //kyu 7
-//Simple Fun #193: Moment Of Time In Space
-function momentOfTimeInSpace(moment) {
-  var time = moment.split('').filter(a=>/[1-9]/.test(a)).reduce((a,b)=>+a+ +b,0)
-  var space = moment.split('').filter(a=>!/[1-9]/.test(a)).length
- return [time<space,time==space,time>space]
-}
