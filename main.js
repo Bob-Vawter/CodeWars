b4fe9df246167b81ae165d902b480f4727401a37
Bob Vawter <bobvawter@msn.com>
2022-05-02 17:07:50 -0400

    cw daily

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 //kyu 7
-//Sum of numerous arguments
-//After calling the function findSum()
-//with any number of non-negative integer arguments
-// it should return the sum of all those arguments.
-//If no arguments are given, the function should return 0
-// if negative arguments are given, it should return -1.
-function findSum(...args){
-  var negs = args.filter(a=>a<0)
-  return negs.length?-1:args.reduce((a,b)=>a+b,0)
-}
+//
